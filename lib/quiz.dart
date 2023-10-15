d90cbbf5f112a8f98a7bb7b86eb6f696af385de9
Enny-Fab <141500196+Enny-Fab@users.noreply.github.com>
2023-10-15 19:19:11 +0100

    Add files via upload

---
+++ lib/quiz.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+
+  // Widget? activeScreen;
+
+  // @override
+  //   void initState() {
+  //     super.initState();
+  //     activeScreen = StartScreen(switchScreen);
+  //   } alternative to the one above
+
+  void switchScreen() {
+    setState(() {
+      activeScreen =
+          'questions-screen'; //using the alternative it will be = const QuestionsScreen();
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    // final screenWidget = activeScreen == 'start-screen'
+    //           ? StartScreen(switchScreen)
+    //           : const QuestionsScreen();
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Color.fromARGB(255, 122, 11, 142),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child:
+              screenWidget, //activeScreen, will be the value for child when using the alternative
+        ),
+      ),
+    );
+  }
+}
